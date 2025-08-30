d0b2ee1e1b55f2c0b7b02a212d5bf5585c1b6875
Erica Phelps <erica@blackspork.com>
2025-08-30 17:47:17 -0500

    chore:Updated basic module settings

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,7 +1,7 @@
 declare const __DEV__: boolean;
 declare const __MODULE_TITLE__: string;
 // declare const __MODULE_ID__: string;
-const __MODULE_ID__ = "module-template";
+const __MODULE_ID__ = "sprite-shadows";
 declare const __MODULE_VERSION__: string;
 
 
