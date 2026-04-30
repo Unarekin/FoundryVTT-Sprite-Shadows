4660805a50aeb9e255e4905170bc548a64183303
Erica Phelps <erica@blackspork.com>
2026-04-29 19:22:44 -0500

    chore:Build typedefs

---
+++ types/applications/StandaloneTokenConfig.d.ts
@@ -0,0 +1,15 @@
+import { DeepPartial, ShadowConfigSource, ShadowConfiguration, ShadowedObject } from "../types";
+import { GlobalConfig } from "./GlobalConfig";
+import { ContextShadowConfiguration } from "./types";
+export declare class StandaloneTokenConfig extends GlobalConfig {
+    protected placeable: ShadowedObject<foundry.canvas.placeables.Token>;
+    static DEFAULT_OPTIONS: DeepPartial<foundry.applications.api.ApplicationV2.Configuration>;
+    overrideShadowConfigSource: ShadowConfigSource | undefined;
+    protected getShadowFlags(): ShadowConfiguration;
+    protected setShadowFlags(config: ShadowConfiguration): Promise<void>;
+    protected toggleSceneSource(enabled: boolean): void;
+    protected loadShadowConfigSettings(source: ShadowConfigSource): Promise<void>;
+    _onRender(context: ContextShadowConfiguration, options: foundry.applications.api.ApplicationV2.RenderOptions): Promise<void>;
+    _prepareContext(options: foundry.applications.api.ApplicationV2.RenderOptions): Promise<ContextShadowConfiguration>;
+    constructor(placeable: ShadowedObject<foundry.canvas.placeables.Token>, options?: foundry.applications.api.ApplicationV2.Configuration);
+}
